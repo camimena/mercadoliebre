8419d083927dda47a26fa43832976a2a4ed6b4e7
camimena <lmgb.camila.mena@gmail.com>
2023-09-19 20:33:56 -0300

    actualizado

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path"); 
-const port = 3000;
+
 
 app.use(express.static(path.join(__dirname,"public")));
 
@@ -16,6 +16,7 @@ app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname,"./views/login.html"));
 })
 
+const port = process.env.PORT ||3030;
 app.listen(port, () =>{
     console.log(`Escuchando en el puerto ${port}`)
     
